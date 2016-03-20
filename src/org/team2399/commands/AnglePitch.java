810f8b51a743974d8d28f3ad06a52ff05d225c61
laurenegts <lauren.egts@gmail.com>
2016-03-20 16:00:42 -0400

    made AnglePitch finish

--- src/org/team2399/commands/AnglePitch.java
+++ src/org/team2399/commands/AnglePitch.java
@@ -54,7 +54,7 @@ public class AnglePitch extends Command
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished()
 	{
-		return false;
+		return true;
 	}
 
 	// Called once after isFinished returns true
